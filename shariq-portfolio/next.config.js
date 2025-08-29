afaa719211b0f6e9ef378282d50be91314698192
shariqsk <00khanshariq@gmail.com>
2025-08-28 21:16:34 -0400

    Remove sitemap.xml redirect configuration from next.config.js to streamline site settings and eliminate unnecessary redirects.

--- shariq-portfolio/next.config.js
+++ shariq-portfolio/next.config.js
@@ -30,21 +30,6 @@ const nextConfig = {
       },
     ];
   },
-  async redirects() {
-    return [
-      {
-        source: '/sitemap.xml',
-        has: [
-          {
-            type: 'host',
-            value: 'www.shariqsafdarkhan.com',
-          },
-        ],
-        destination: 'https://shariqsafdarkhan.com/sitemap.xml',
-        permanent: true,
-      },
-    ];
-  },
 };
 
 module.exports = nextConfig;
